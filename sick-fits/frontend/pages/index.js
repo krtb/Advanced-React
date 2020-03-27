bd5c71286bc71d147e79baf6ced3a405eebe8dc1
Kurt Bauer <kurtbauer55@protonmail.com>
2020-03-27 11:28:09 -0400

    add Link tags from sell to home and home to sell

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'; // could delete, because Next.js will take care of this
+import Link from 'next/link';
 
 const Home = () => (
  <div>
      <p>
         index page!
      </p>
+     <Link href="/sell" >Sell Page</Link>
  </div>
 )
 
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,10 +1,14 @@
 import React from 'react'; // could delete, because Next.js will take care of this
+import Link from 'next/link';
 
 const Sell = () => (
     <div>
         <p>
         Sell items page!
         </p>
+        <Link href="/index" >
+            Home!
+        </Link>
     </div>
 )
 
