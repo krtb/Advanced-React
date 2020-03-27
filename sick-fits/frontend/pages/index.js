dcc9ba1bfc31bfbeeb511c9c5ea5694560446378
Kurt Bauer <kurtbauer55@protonmail.com>
2020-03-27 11:24:16 -0400

    create two new pages in pages folder

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'; // could delete, because Next.js will take care of this
+
+const Home = () => (
+ <div>
+     <p>
+        index page!
+     </p>
+ </div>
+)
+
+export default Home
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import React from 'react'; // could delete, because Next.js will take care of this
+
+const Sell = () => (
+    <div>
+        <p>
+        Sell items page!
+        </p>
+    </div>
+)
+
+export default Sell
