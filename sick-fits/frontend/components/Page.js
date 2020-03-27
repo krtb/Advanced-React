230e483da7c0e1ecd1a279ff4aebd2e95c98036c
Kurt Bauer <kurtbauer55@protonmail.com>
2020-03-27 19:19:28 -0400

    add ThemeProvider, injectGlobal, styled-components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+// Components
 import Header from '../components/Header';
+import Meta from './Meta';
+
+// regular object
+const theme = {
+    red: '#FF0000',
+    black: '#393939',
+    grey: '#3A3A3A',
+    lightgrey: '#E1E1E1',
+    offWhite: '#EDEDED',
+    maxWidth: '1000px',
+    bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+// tag template literal // using black from (theme) above
+const StyledPage = styled.div`
+    background: white;
+    color: ${props => props.theme.black};
+`;
+
+// props is making use of interpolation
+const Inner = styled.div`
+    max-width: ${props => props.theme.maxWidth};
+     margin: 0 auto;
+    padding: 2rem;
+`;
 
 class Page extends Component {
-    render() {
+    render() { 
         return (
-            <div>
+            <ThemeProvider theme={theme} >
+            <StyledPage>
+                <Meta/>
                 <Header /> 
-                {this.props.children}
-            </div>
+                <Inner>{this.props.children}</Inner>
+            </StyledPage>
+            </ThemeProvider>
         )
     }
 }
