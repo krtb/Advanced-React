562cb480339ba70e85bc9829b397461c60b412d0
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-02 17:55:57 -0400

    create function for new GraphQlServer instance

--- sick-fits/backend/src/createServer.js
+++ sick-fits/backend/src/createServer.js
@@ -1,1 +1,23 @@
 const { GraphQlServer } = require('graphql-yoga');
+// RESOLVERS
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+// DATABASE
+const db = require('./db');
+// CREATE GraphQlYoga Server
+
+function createServer(params) {
+    return new GraphQlServer({
+        typedefs: './schema.graphql',
+        resolervers: {
+            Mutation,
+            Query
+        },
+        resolverValidationOptions: {
+            requireResolversForResolverType: false
+        },
+        context: req => ({ ...req, db })
+    })
+}
+
+module.export = createServer;
