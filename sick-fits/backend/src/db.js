90c048adbf7a6912ad9fdddbca9f9e4f9ea8e6be
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-02 17:38:54 -0400

    connect to remote prisma db with graphql yoga

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,14 @@
+// CONNECTS TO REMOTE PRISMA DB
+// and GIVES ABILITY TO QUERY WITH JS
+const { Prisma } = require('prisma-binding'); 
+// using require, was no import in Node. 
+// Could use Babel or ESM to import. This is latest Node.js standard
+
+const db = new Prisma({
+    typeDefs: './generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false
+})
+
+module.exports = db
